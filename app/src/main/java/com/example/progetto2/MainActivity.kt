3f67f5fb7b28c57285760cc65542f41901d8cda5
GiustFR98 <giustino.esposito@studenti.unicampania.it>
2019-05-25 21:43:28 +0200

    Creata schermata di login e aggiunto il collegamento con firebase

--- app/src/main/java/com/example/progetto2/MainActivity.kt
+++ app/src/main/java/com/example/progetto2/MainActivity.kt
@@ -2,18 +2,13 @@ package com.example.progetto2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.Toolbar
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_ps4_list.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
         // Imposta il menu dal file di risorse
-        menuInflater.inflate(R.menu.button_impostazioni, menu)
+        menuInflater.inflate(R.menu.button_login, menu)
 
         return true
     }
@@ -37,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
 
         when(item?.itemId) {
-            R.id.fragment_impostazioni -> Navigation.findNavController(this, R.id.navHost).navigate(R.id.action_home_to_fragment_impostazioni)
+            R.id.fragment_login -> Navigation.findNavController(this, R.id.navHost).navigate(R.id.action_home_to_fragment_impostazioni)
             else -> return false    // Voce non processata
         }
 
--- app/src/main/java/com/example/progetto2/fragment_login.kt
+++ app/src/main/java/com/example/progetto2/fragment_login.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.text.method.TextKeyListener.clear
+import android.util.Log
 import android.view.*
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.fragment_fragment_login.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -27,6 +31,9 @@ class fragment_impostazioni : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
+    var auth = FirebaseAuth.getInstance()
+    private val TAG = "MainActivity"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,13 +44,73 @@ class fragment_impostazioni : Fragment() {
         //aggiungo questa riga per aggiungere un riferimento al menu
         setHasOptionsMenu(true)
 
+
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_impostazioni, container, false)
+        return inflater.inflate(R.layout.fragment_fragment_login, container, false)
+    }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.getCurrentUser()
+        updateUI(currentUser)
+    }
+    fun updateUI(usr : FirebaseUser?){
+        if (usr!= null){
+            Toast.makeText(activity,"Utente loggato", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun createAccount(email : String, password : String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(MainActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(activity, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+    fun signIn(email : String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(MainActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("MainActivity", "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("MainActivity", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(activity,"Autenticazione fallita", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnConferma.setOnClickListener{
+        signIn(email.text.toString(), password.text.toString())
+        }
     }
 
     //questa funzione rende invisibile il menu nel fragment impostazioni
