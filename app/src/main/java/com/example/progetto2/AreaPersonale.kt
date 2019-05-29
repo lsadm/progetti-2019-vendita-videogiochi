4599833c3bf9f40acd70d67272a8a475ece733ee
GiustFR98 <giustino.esposito@studenti.unicampania.it>
2019-05-29 04:00:49 +0200

    Aggiunta sezione area personale funzionante.

---
+++ app/src/main/java/com/example/progetto2/AreaPersonale.kt
@@ -0,0 +1,148 @@
+package com.example.progetto2
+
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.progetto2.datamodel.Gioco
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.fragment_area_personale.*
+import kotlinx.android.synthetic.main.fragment_ps4_list.*
+import java.lang.Exception
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [AreaPersonale.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [AreaPersonale.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class AreaPersonale : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    lateinit var database : DatabaseReference
+    lateinit var auth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+        database = FirebaseDatabase.getInstance().getReference("users")
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_area_personale, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        super.onViewCreated(view, savedInstanceState)
+        val v: View? = activity?.findViewById(R.id.bottomNavigation)
+        v?.visibility=View.VISIBLE
+        val games=ArrayList<Gioco?>()
+        val adapter = Adapter(games,requireContext())
+        lista_mieigiochi.adapter = adapter
+
+        val childEventListener = object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildAdded:" + dataSnapshot.key!!)
+
+                // A new comment has been added, add it to the displayed list
+                val g = dataSnapshot.getValue(Gioco::class.java)
+                games.add(g)
+                adapter.notifyDataSetChanged()
+
+                // ...
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildChanged: ${dataSnapshot.key}")
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so displayed the changed comment.
+                val newComment = dataSnapshot.getValue(Gioco::class.java)
+                val commentKey = dataSnapshot.key
+
+                // ...
+            }
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + dataSnapshot.key!!)
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so remove it.
+                val commentKey = dataSnapshot.key
+
+                // ...
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildMoved:" + dataSnapshot.key!!)
+
+                // A comment has changed position, use the key to determine if we are
+                // displaying this comment and if so move it.
+                val movedComment = dataSnapshot.getValue(Gioco::class.java)
+                val commentKey = dataSnapshot.key
+
+                // ...
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "postComments:onCancelled", databaseError.toException())
+                Toast.makeText(context, "Failed to load comments.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+        try {
+            database.child(auth.currentUser!!.uid)
+                .addChildEventListener(childEventListener)    //il database da cui chiamo il listener fa variare il sottonodo del database che vado a leggere
+        }
+        catch (e : Exception){
+            Toast.makeText(activity,"Non sei loggato", Toast.LENGTH_SHORT).show()
+        }
+
+
+        // Imposto il layout manager a lineare per avere scrolling in una direzione
+        lista_mieigiochi.layoutManager = LinearLayoutManager(activity)
+
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+
+
+}
