1f2b95304816c78b65151cf8575af90bd53fd91e
GiustFR98 <giustino.esposito@studenti.unicampania.it>
2019-06-10 18:38:31 +0200

    Aggiunto inserimento e visualizzazione di più foto

--- app/src/main/java/com/example/progetto2/fragment_inserimento.kt
+++ app/src/main/java/com/example/progetto2/fragment_inserimento.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v4.app.Fragment
 import android.view.*
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.progetto2.datamodel.Gioco
@@ -28,7 +29,7 @@ class fragment_inserimento : Fragment() {
         setHasOptionsMenu(true)
     }
     // Costante utilizzata per distinguere l'origine della richiesta
-    val REQUEST_IMAGE_CAPTURE = 1
+    var REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,18 +50,32 @@ class fragment_inserimento : Fragment() {
         val v: View? = activity?.findViewById(R.id.bottomNavigation)
         v?.visibility=View.GONE
         // Imposta il funzionamento del pulsante per l'acqisizione dell'immagine
+        val takePhoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         foto1.setOnClickListener {
+            REQUEST_IMAGE_CAPTURE = 1
             // Creo un intent di tipo implicito per acquisire l'immagine
-            val takePhoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             takePhoto.resolveActivity(activity!!.packageManager)?.also {
                 startActivityForResult(takePhoto, REQUEST_IMAGE_CAPTURE)
             }
         }
+        foto2.setOnClickListener {
+            REQUEST_IMAGE_CAPTURE = 2
+            // Creo un intent di tipo implicito per acquisire l'immagine
+            takePhoto.resolveActivity(activity!!.packageManager)?.also {
+                startActivityForResult(takePhoto, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+        foto3.setOnClickListener {
+            REQUEST_IMAGE_CAPTURE = 3
+            // Creo un intent di tipo implicito per acquisire l'immagine
+            takePhoto.resolveActivity(activity!!.packageManager)?.also {
+                startActivityForResult(takePhoto, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+
     }
     //inserimento annuncio, con tasto in alto a destra
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.ps4_list -> {      // Conferma
                 val nome = nome_gioco.text.toString()
                 val luogo = luogo_gioco.text.toString()
                 val prezzo = prezzo_gioco.text.toString()
@@ -81,21 +96,7 @@ class fragment_inserimento : Fragment() {
                                 key.toString()
                             )
                         )    //in quel percorso con identificativo unico inserisco il gioco , rappresenta la lista giochi visibile a tutti
-                        val storageRef = FirebaseStorage.getInstance().getReference()
-                        // Create a reference to "mountains.jpg",è il nome del file che stiamo caricando
-                        val mountainsRef = storageRef.child(key.toString())
-
-                        val bitmap = (foto1.drawable as? BitmapDrawable)?.bitmap
-                        val baos = ByteArrayOutputStream()
-                        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-                        val data = baos.toByteArray()
-                        var uploadTask = mountainsRef.putBytes(data) //carica i byte della foto
-                        uploadTask.addOnFailureListener {
-                            Toast.makeText(activity,"Foto non inserita correttamente",Toast.LENGTH_SHORT).show()
-                        }.addOnSuccessListener {
-                            //Toast.makeText(activity,"Foto inserita correttamente",Toast.LENGTH_SHORT).show()
-                            //non mi serve a nulla
-                        }
+                        caricaFoto(key.toString())
                     }
                     if (checkXbox.isChecked) {
                         key = database.child("Giochi").child("Xbox")
@@ -108,21 +109,7 @@ class fragment_inserimento : Fragment() {
                                 key.toString()
                             )
                         )    //in quel percorso con identificativo unico inserisco il gioco , rappresenta la lista giochi visibile a tutti
-                        val storageRef = FirebaseStorage.getInstance().getReference()
-                        // Create a reference to "mountains.jpg",è il nome del file che stiamo caricando
-                        val mountainsRef = storageRef.child(key.toString())
-
-                        val bitmap = (foto1.drawable as? BitmapDrawable)?.bitmap
-                        val baos = ByteArrayOutputStream()
-                        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-                        val data = baos.toByteArray()
-                        var uploadTask = mountainsRef.putBytes(data) //carica i byte della foto
-                        uploadTask.addOnFailureListener {
-                            Toast.makeText(activity,"Foto non inserita correttamente",Toast.LENGTH_SHORT).show()
-                        }.addOnSuccessListener {
-                            //Toast.makeText(activity,"Foto inserita correttamente",Toast.LENGTH_SHORT).show()
-                            //non mi serve a nulla
-                        }
+                        caricaFoto(key.toString())
                     }
                     if (checkNintendo.isChecked) {
                          key = database.child("Giochi").child("Nintendo")
@@ -135,21 +122,7 @@ class fragment_inserimento : Fragment() {
                                 key.toString()
                             )
                         )    //in quel percorso con identificativo unico inserisco il gioco , rappresenta la lista giochi visibile a tutti
-                        val storageRef = FirebaseStorage.getInstance().getReference()
-                        // Create a reference to "mountains.jpg",è il nome del file che stiamo caricando
-                        val mountainsRef = storageRef.child(key.toString())
-
-                        val bitmap = (foto1.drawable as? BitmapDrawable)?.bitmap
-                        val baos = ByteArrayOutputStream()
-                        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-                        val data = baos.toByteArray()
-                        var uploadTask = mountainsRef.putBytes(data) //carica i byte della foto
-                        uploadTask.addOnFailureListener {
-                            Toast.makeText(activity,"Foto non inserita correttamente",Toast.LENGTH_SHORT).show()
-                        }.addOnSuccessListener {
-                            //Toast.makeText(activity,"Foto inserita correttamente",Toast.LENGTH_SHORT).show()
-                            //non mi serve a nulla
-                        }
+                        caricaFoto(key.toString())
                     }
                     database.child("users").child(id).child(nome).setValue(Gioco(nome, prezzo.toInt(), luogo,key.toString()))   //carico nel database nell'area riservata
                     Toast.makeText(activity,"Gioco inserito correttamente",Toast.LENGTH_SHORT).show()
@@ -160,10 +133,32 @@ class fragment_inserimento : Fragment() {
                 else { //se alcuni campi sono vuoti non posso caricare il gioco
                         Toast.makeText(activity,"Hai mancato qualche campo", Toast.LENGTH_SHORT).show()
                      }
-                }
-            }
+
 
         return super.onOptionsItemSelected(item)
+    }
+
+    fun caricaFoto(key : String){
+        val storageRef = FirebaseStorage.getInstance().getReference()
+        // Create a reference to "mountains.jpg",è il nome del file che stiamo caricando
+        val foto = ArrayList<ImageButton>() //array usato per inserire 3 foto
+        foto.add(foto1)
+        foto.add(foto2)
+        foto.add(foto3)
+        for (i in 0 .. 2) {
+            val mountainsRef = storageRef.child(key).child("picture" + i.toString())
+            val bitmap = (foto.get(i).drawable as? BitmapDrawable)?.bitmap
+            val baos = ByteArrayOutputStream()
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val data = baos.toByteArray()
+            var uploadTask = mountainsRef.putBytes(data) //carica i byte della foto
+            uploadTask.addOnFailureListener {
+                Toast.makeText(activity, "Foto non inserita correttamente", Toast.LENGTH_SHORT).show()
+            }.addOnSuccessListener {
+                //Toast.makeText(activity,"Foto inserita correttamente",Toast.LENGTH_SHORT).show()
+                //non mi serve a nulla
+            }
+        }
     }
     /**
      * Questo metodo viene invocato per gestire il risultato al ritorno da una activity
@@ -173,7 +168,11 @@ class fragment_inserimento : Fragment() {
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {     // Acquisizione immagine
             val immagineCatturata = data?.extras?.get("data") as Bitmap
-            foto1.setImageBitmap(immagineCatturata)
+           when(REQUEST_IMAGE_CAPTURE){
+               1 -> foto1.setImageBitmap(immagineCatturata)
+               2 -> foto2.setImageBitmap(immagineCatturata)
+               3 -> foto3.setImageBitmap(immagineCatturata)
+           }
         }
     }
 }
