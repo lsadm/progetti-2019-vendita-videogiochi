efaf650db725b6065a68cb2a2d74d0b57e9e69aa
umberto <umberto.gagliardini@studenti.unicampania.it>
2019-06-14 11:37:45 +0200

    Aggiunti collegamenti rapidi a email e telefono in dettaglio gioco

--- app/src/main/java/com/example/progetto2/dettaglio_gioco.kt
+++ app/src/main/java/com/example/progetto2/dettaglio_gioco.kt
@@ -4,7 +4,9 @@ package com.example.progetto2
 import android.app.AlertDialog
 import android.content.ContentValues.TAG
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
@@ -161,6 +163,51 @@ class dettaglio_gioco : Fragment() {
                     }
                 }
             }
+        }
+        //invio email
+        utente_dettaglio.setOnClickListener {
+            //get input from EditTexts and save in variables
+            val recipient = utente_dettaglio.text.toString().trim()
+            val subject = "BuyGames".trim()
+            val message = "Ciao".trim()
+            //method call for email intent with these inputs as parameters
+            sendEmail(recipient, subject, message)
+        }
+        cellulare_dettaglio.setOnClickListener {
+            val seller = cellulare_dettaglio.text.toString()
+            callCell(seller)
+        }
+    }
+
+    //funzione per chiamare proprietario gioco
+    private fun callCell(seller : String) {
+        val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel",seller,null))
+        startActivity(intent)
+    }
+
+    //funzione che svolge l'invio dell'email
+    private fun sendEmail(recipient: String, subject: String, message: String) {
+        val mIntent = Intent(Intent.ACTION_SEND)
+        /*To send an email you need to specify mailto: as URI using setData() method
+        and data type will be to text/plain using setType() method*/
+        mIntent.data = Uri.parse("mailto:")
+        mIntent.type = "text/plain"
+        // put recipient email in intent
+        /* recipient is put as array because you may wanna send email to multiple emails
+           so enter comma(,) separated emails, it will be stored in array*/
+        mIntent.putExtra(Intent.EXTRA_EMAIL,arrayOf(recipient))
+        //put the Subject in the intent
+        mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        //put the message in the intent
+        mIntent.putExtra(Intent.EXTRA_TEXT, message)
+        try {
+            //start email intent
+            startActivity(Intent.createChooser(mIntent, "Choose Email Client..."))
+        }
+        catch (e: Exception){
+            //if any thing goes wrong for example no email client application or any exception
+            //get and show exception message
+            Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
         }
     }
 }
